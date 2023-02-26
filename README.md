30f5cd3d53aadc8a3d8645ad96e9b5d2b73cc5e0
David Michel <david.michel2809@gmail.com>
2023-02-26 14:15:58 +0100

    feat: add 9Gag_control_videos script

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 
 | Link | Description |
 | -- | -- |
+| [9Gag automatic display video controls](./src/9Gag_control_videos/9Gag_control_videos.js) | Show controls by default on 9gag videos |
---
+++ src/9Gag_control_videos/9Gag_control_videos.js
@@ -0,0 +1,38 @@
+"use strict";
+(function () {
+    console.log("9Gag_control_videos script loaded");
+    const observer = new MutationObserver((records) => {
+        records.forEach(({ target }) => {
+            if (target instanceof HTMLElement) {
+                const targetContainsVideo = (target.classList.contains("video-post") ||
+                    target.classList.contains("gif-post"));
+                if (targetContainsVideo) {
+                    const videoElement = target.children[0];
+                    handleVideo(videoElement);
+                }
+            }
+        });
+    });
+    /**
+     * Handle settings of the video element such as controls and volume
+     *
+     */
+    function handleVideo(videoElement) {
+        videoElement.controls = true;
+        videoElement.volume = 0.2;
+    }
+    let contentElement = null;
+    /*  Check every 200ms if list-view-2 class element is available
+      Once available, stop interval and apply observer */
+    const interval = setInterval(() => {
+        contentElement = document.getElementById("list-view-2");
+        if (contentElement !== null) {
+            clearInterval(interval);
+            observer.observe(contentElement, {
+                childList: true,
+                subtree: true,
+                attributes: false
+            });
+        }
+    }, 200);
+})();
