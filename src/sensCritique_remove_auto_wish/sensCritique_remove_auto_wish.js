c1dbb11d473c6c0d483fd605e0c3229e1a818364
David Michel <david.michel2809@gmail.com>
2024-01-16 08:03:15 +0100

    refacto: global refacto on sensCritique_remove_auto_wish

--- src/sensCritique_remove_auto_wish/sensCritique_remove_auto_wish.js
+++ src/sensCritique_remove_auto_wish/sensCritique_remove_auto_wish.js
@@ -15,33 +15,28 @@
         }
         return markMovieAsWatchedButton;
     }
-    const markMovieAsWatchedButton = await getMarkMovieAsWatchedButton();
-    console.log(markMovieAsWatchedButton);
-    if (markMovieAsWatchedButton === null) {
-        return console.log("Le bouton 'Vu' n'a pas été trouvé");
-    }
-    async function handleClick() {
-        const authCookie = getAuthCookie();
-        if (authCookie === null) {
-            return console.log("cookie d'authentification non trouvé");
+    class Helpers {
+        static getMovieId() {
+            const pageHref = location.href;
+            const movieIdRegex = /\d+$/;
+            const authCookieMatch = pageHref.match(movieIdRegex);
+            return authCookieMatch && authCookieMatch[0];
         }
-        const movieId = getMovieId();
-        if (movieId === null) {
-            return console.log("L'id du film n'a pas été trouvé");
+        static async wait(time) {
+            return new Promise((resolve) => {
+                setTimeout(resolve, time);
+            });
         }
-        await wait(200);
-        removeMovieFromWishList(movieId, authCookie);
-    }
-    function getMovieId() {
-        const pageHref = location.href;
-        const movieIdRegex = /\d+$/;
-        const authCookieMatch = pageHref.match(movieIdRegex);
-        return authCookieMatch && authCookieMatch[0];
-    }
-    async function wait(time) {
-        return new Promise((resolve) => {
-            setTimeout(resolve, time);
-        });
+        static getAuthCookie() {
+            const cookie = document.cookie;
+            const regex = /(?<=SC_AUTH=)[^\s]+/;
+            const authCookieMatch = cookie.match(regex);
+            return authCookieMatch && authCookieMatch[0];
+        }
+        static checkIfCurrentPageIsAMoviePage() {
+            const moviePathRegex = /\/film\/.+\/\d+$/;
+            return moviePathRegex.test(location.pathname);
+        }
     }
     async function removeMovieFromWishList(movieId, authCookie) {
         const body = `[{ 
@@ -63,24 +58,30 @@
             body: `${body}`,
         });
     }
-    function getAuthCookie() {
-        const cookie = document.cookie;
-        const regex = /(?<=SC_AUTH=)[^\s]+/;
-        const authCookieMatch = cookie.match(regex);
-        return authCookieMatch && authCookieMatch[0];
+    async function handleMarkMovieAsWatchedButtonClick() {
+        const authCookie = Helpers.getAuthCookie();
+        if (authCookie === null) {
+            return console.log("cookie d'authentification non trouvé");
+        }
+        const movieId = Helpers.getMovieId();
+        if (movieId === null) {
+            return console.log("L'id du film n'a pas été trouvé");
+        }
+        await Helpers.wait(200);
+        removeMovieFromWishList(movieId, authCookie);
     }
-    markMovieAsWatchedButton.addEventListener("click", handleClick);
+    const markMovieAsWatchedButton = await getMarkMovieAsWatchedButton();
+    if (markMovieAsWatchedButton === null) {
+        return console.log("Le bouton 'Vu' n'a pas été trouvé");
+    }
+    markMovieAsWatchedButton.addEventListener("click", handleMarkMovieAsWatchedButtonClick);
     let previousPathname = location.pathname;
     const mutationObserver = new MutationObserver(() => {
         if (previousPathname !== location.pathname) {
             previousPathname = location.pathname;
-            console.log("new path page");
-            markMovieAsWatchedButton.removeEventListener("click", handleClick);
-            const moviePathRegex = /\/film\/.+\/\d+$/;
-            const isOnAMoviePage = moviePathRegex.test(location.pathname);
-            if (isOnAMoviePage === false)
+            markMovieAsWatchedButton.removeEventListener("click", handleMarkMovieAsWatchedButtonClick);
+            if (Helpers.checkIfCurrentPageIsAMoviePage() === false)
                 return;
-            console.log("movie page");
             mutationObserver.disconnect();
             handler();
         }
--- src/sensCritique_remove_auto_wish/sensCritique_remove_auto_wish.ts
+++ src/sensCritique_remove_auto_wish/sensCritique_remove_auto_wish.ts
@@ -18,39 +18,32 @@
     return markMovieAsWatchedButton
   }
 
-  const markMovieAsWatchedButton = await getMarkMovieAsWatchedButton()
-  console.log(markMovieAsWatchedButton)
-
-  if (markMovieAsWatchedButton === null) {
-    return console.log("Le bouton 'Vu' n'a pas été trouvé")
-  }
-
-  async function handleClick() {
-    const authCookie = getAuthCookie()
-    if (authCookie === null) {
-      return console.log("cookie d'authentification non trouvé")
+  class Helpers {
+    static getMovieId() {
+      const pageHref = location.href
+      const movieIdRegex = /\d+$/
+      const authCookieMatch = pageHref.match(movieIdRegex)
+      return authCookieMatch && authCookieMatch[0]
     }
 
-    const movieId = getMovieId()
-    if (movieId === null) {
-      return console.log("L'id du film n'a pas été trouvé")
+    static async wait(time: number) {
+      return new Promise((resolve) => {
+        setTimeout(resolve, time)
+      })
     }
 
-    await wait(200)
-    removeMovieFromWishList(movieId, authCookie)
-  }
+    static getAuthCookie() {
+      const cookie = document.cookie
 
-  function getMovieId() {
-    const pageHref = location.href
-    const movieIdRegex = /\d+$/
-    const authCookieMatch = pageHref.match(movieIdRegex)
-    return authCookieMatch && authCookieMatch[0]
-  }
+      const regex = /(?<=SC_AUTH=)[^\s]+/
+      const authCookieMatch = cookie.match(regex)
+      return authCookieMatch && authCookieMatch[0]
+    }
 
-  async function wait(time: number) {
-    return new Promise((resolve) => {
-      setTimeout(resolve, time)
-    })
+    static checkIfCurrentPageIsAMoviePage() {
+      const moviePathRegex = /\/film\/.+\/\d+$/
+      return moviePathRegex.test(location.pathname)
+    }
   }
 
   async function removeMovieFromWishList(movieId: string, authCookie: string) {
@@ -73,32 +66,40 @@
       },
       body: `${body}`,
     });
-
   }
 
-  function getAuthCookie() {
-    const cookie = document.cookie
+  async function handleMarkMovieAsWatchedButtonClick() {
+    const authCookie = Helpers.getAuthCookie()
+    if (authCookie === null) {
+      return console.log("cookie d'authentification non trouvé")
+    }
 
-    const regex = /(?<=SC_AUTH=)[^\s]+/
-    const authCookieMatch = cookie.match(regex)
-    return authCookieMatch && authCookieMatch[0]
+    const movieId = Helpers.getMovieId()
+    if (movieId === null) {
+      return console.log("L'id du film n'a pas été trouvé")
+    }
+
+    await Helpers.wait(200)
+    removeMovieFromWishList(movieId, authCookie)
   }
 
-  markMovieAsWatchedButton.addEventListener("click", handleClick)
+  const markMovieAsWatchedButton = await getMarkMovieAsWatchedButton()
+
+  if (markMovieAsWatchedButton === null) {
+    return console.log("Le bouton 'Vu' n'a pas été trouvé")
+  }
+
+  markMovieAsWatchedButton.addEventListener("click", handleMarkMovieAsWatchedButtonClick)
 
   let previousPathname = location.pathname
 
   const mutationObserver = new MutationObserver(() => {
     if (previousPathname !== location.pathname) {
       previousPathname = location.pathname
-      console.log("new path page")
-      markMovieAsWatchedButton.removeEventListener("click", handleClick)
+      markMovieAsWatchedButton.removeEventListener("click", handleMarkMovieAsWatchedButtonClick)
 
-      const moviePathRegex = /\/film\/.+\/\d+$/
-      const isOnAMoviePage = moviePathRegex.test(location.pathname)
-      if (isOnAMoviePage === false) return
+      if (Helpers.checkIfCurrentPageIsAMoviePage() === false) return
 
-      console.log("movie page")
       mutationObserver.disconnect()
       handler()
     }
