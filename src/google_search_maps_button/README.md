81e7c53ab6ec289c82746913ae3b2e017b58bef0
David Michel <david.michel2809@gmail.com>
2024-12-13 07:56:42 +0100

    feat: add Google Search - Maps button

---
+++ src/google_search_maps_button/README.md
@@ -0,0 +1,13 @@
+# Google Search - Maps button v1.0.0
+
+## Description
+This script brings back the maps button on google search
+
+## Comparison
+Here are two gifs comparing behaviours (with and without script execution) when making a google search
+
+- #### Without script
+![Alt text](doc/assets/original_behaviour.gif)
+
+- #### With script
+![Alt text](doc/assets/new_behaviour.gif)
---
+++ src/google_search_maps_button/google_search_maps_button.ts
@@ -0,0 +1,31 @@
+(function init() {
+  //@ts-ignore
+  const scriptName = (GM_info as GMInfo).script.name;
+  //@ts-ignore
+  const scriptVersion = GM_info.script.version;
+
+  function displayConsoleColoredMessage(message: string, color: string) {
+    return console.info(`%c${scriptName} :%c ${message}`, `color: ${color}; font-weight:bold;`, "color: ''",);
+  }
+
+  const currentSearchQuery = new URL(location.href).searchParams.get("q");
+  const mapsSearchUrl = `/maps/search/${currentSearchQuery}`;
+
+  const mapsButtonTitleElement = document.createElement("div");
+  mapsButtonTitleElement.classList.add("YmvwI");
+  mapsButtonTitleElement.innerText = "Maps";
+
+  const mapsLinkElement = document.createElement("a");
+  mapsLinkElement.href = mapsSearchUrl;
+  mapsLinkElement.classList.add("nPDzT", "T3FoJb");
+  mapsLinkElement.append(mapsButtonTitleElement);
+
+  const mapsButtonWrapper = document.createElement("div");
+  mapsButtonWrapper.setAttribute("role", "listitem");
+  mapsButtonWrapper.append(mapsLinkElement);
+
+  const googleButtonsContainer = document.querySelector(".crJ18e [role='list']") as HTMLDivElement;
+  googleButtonsContainer.insertBefore(mapsButtonWrapper, googleButtonsContainer.children[1]);
+
+  displayConsoleColoredMessage(`Script loaded (v${scriptVersion})`, "#db8d45");
+})();
